f2ca53605c07d3ba238fed21a2ec80dbfb57c15c
KMichaylov <krisidm24@gmail.com>
2023-05-24 22:49:14 +0200

    Embed images of StegHide

---
+++ src/steghide_script.sh
@@ -0,0 +1,20 @@
+# Only embedd in the following images:
+#     • Building
+    # • City
+    # • Food
+    # • Party
+    # • Nature
+    # • Stadium
+    # • River
+    # • Sea
+
+# passphrase - secret$#497
+for FILE in *
+do
+    if [[ "$FILE" == "secret.txt" ]] ; then
+        continue
+    fi
+    filename="${FILE%%.*}"
+    steghide embed -cf $filename.jpg -ef secret.txt -sf $filename-stego.jpg
+    rm $FILE
+done
