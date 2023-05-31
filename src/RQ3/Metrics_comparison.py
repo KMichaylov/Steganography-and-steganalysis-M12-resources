08098a902a11315fb30cbace1fda5843f4bff16f
Kristiyan Michaylov <krisidm24@gmail.com>
2023-05-31 11:57:29 +0200

    Include MSE instead of entropy

--- src/RQ3/Metrics_comparison.py
+++ src/RQ3/Metrics_comparison.py
@@ -1,10 +1,21 @@
 import cv2
 from tabulate import tabulate
 import os
+import numpy as np
+
+
+def MSE(cover_image, stego_image):
+    h, w, _ = cover_image.shape
+    diff = cv2.subtract(cover_image, stego_image)
+    err = np.sum(diff ** 2)
+    mse = err / (float(h * w))
+    return mse
+
 
 # TODO: Create new dataset including at least 2 other different sizes and analyse the results.
 # Ask Dipti for other sizes.
-columns = ["Name", "Size", "Dimensions" "Color", "Entropy", "PSNR"]
+columns = ["Name", "Size", "Dimensions" "Color", "MSE", "PSNR"]
+# Create a flexible script, independent of folder location and images,
 folder_location = "D:\Study Materials\Year 3 (2022-2023)\Module " \
                   "12\Resources\Experiment_code\Steganography-and-steganalysis-M12-resources\images\RQ3-dataset" \
                   "\Images512x512\F5\m1-images"
@@ -15,6 +26,7 @@ data = []
 images = os.listdir(folder_location)
 os.chdir(folder_location)
 current_psnr = 0
+current_mse = 0
 for i in range(0, len(images) - 1):
     img_location = "D:\Study Materials\Year 3 (2022-2023)\Module " \
                    "12\Resources\Experiment_code\Steganography-and-steganalysis-M12-resources\images\RQ3-dataset" \
@@ -31,12 +43,17 @@ for i in range(0, len(images) - 1):
     else:
         row.append('colourful')
     if count % 2 == 0:
+        mse = MSE(img1, img2)
         psnr = cv2.PSNR(img1, img2)
         current_psnr = psnr
+        current_mse = mse
     else:
+        mse = current_mse
         psnr = current_psnr
-    count +=1
-    row.append("Entropy still unknown")
+    count += 1
+    row.append(mse)
     row.append(psnr)
     data.append(row)
 print(tabulate(data, headers=columns, tablefmt="grid", showindex="always"))
+
+# The code is from GeeksForGeeks: https://www.tutorialspoint.com/how-to-compare-two-images-in-opencv-python
