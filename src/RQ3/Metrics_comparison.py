65353a3101788caa3567afde97af5646bdf1e592
Kristiyan Michaylov <krisidm24@gmail.com>
2023-06-04 16:55:17 +0200

    Fix error in RQ3 experiment

--- src/RQ3/Metrics_comparison.py
+++ src/RQ3/Metrics_comparison.py
@@ -10,7 +10,7 @@ images_root_location = os.path.join("..", "..", "images", "RQ3-dataset", "Images
 os.chdir(images_root_location)
 image_dataset_names = ["F5", "Outguess", "Steghide"]
 steganography_images_folder = ["m1-images", "m2-images", "m3-images", "m4-images", "m5-images"]
-columns = ["Name", "Size", "Dimensions" "Color", "MSE", "PSNR"]
+columns = ["Name", "Size", "Dimensions", "Color", "MSE", "PSNR"]
 for image_dataset in image_dataset_names:
     new_path = os.path.join(os.getcwd(), image_dataset)
     os.chdir(new_path)
@@ -27,37 +27,50 @@ for image_dataset in image_dataset_names:
         with open(location_for_results, 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Name", "Size", "Dimensions" "Color", "MSE", "PSNR"])
-            for i in range(0, len(images) - 2):
+            for i in range(0, len(images)):
                 path_to_image = os.path.join(os.getcwd(), images[i])
                 img_location = "{}".format(path_to_image)
                 row = list()
-                # Get correct stego and cover image
-                if count % 2 == 0:
+                # Only different for last item
+                if i == len(images) - 1:
                     img1 = cv2.imread(images[i])
-                    img2 = cv2.imread(images[i + 3])
-                elif count % 2 == 1:
-                    img1 = cv2.imread(images[i])
-                    img2 = cv2.imread(images[i + 1])
-                row.append(images[i])
-                row.append("%.2f" % (os.path.getsize(img_location)) + " B")
-                row.append(str(img1.shape[0]) + "X" + str(img1.shape[1]))
-                if "-g" in images[i]:
-                    row.append('grayscale')
-                else:
-                    row.append('colourful')
-                if count % 2 == 0:
-                    mse = MSE(img1, img2)
-                    psnr = cv2.PSNR(img1, img2)
-                    current_psnr = psnr
-                    current_mse = mse
-                else:
+                    row.append(images[i])
+                    row.append("%.2f" % (os.path.getsize(img_location)) + " B")
+                    row.append(str(img1.shape[0]) + "X" + str(img1.shape[1]))
+                    if "-g" in images[i]:
+                        row.append('grayscale')
+                    else:
+                        row.append('colourful')
                     mse = current_mse
                     psnr = current_psnr
-                count += 1
-                row.append(mse)
-                row.append(psnr)
-                data.append(row)
-                writer.writerow(row)
+                    count += 1
+                    row.append(mse)
+                    row.append(psnr)
+                    data.append(row)
+                    writer.writerow(row)
+                else:
+                    img1 = cv2.imread(images[i])
+                    img2 = cv2.imread(images[i + 1])
+                    row.append(images[i])
+                    row.append("%.2f" % (os.path.getsize(img_location)) + " B")
+                    row.append(str(img1.shape[0]) + "X" + str(img1.shape[1]))
+                    if "-g" in images[i]:
+                        row.append('grayscale')
+                    else:
+                        row.append('colourful')
+                    if count % 2 == 0:
+                        mse = MSE(img1, img2)
+                        psnr = cv2.PSNR(img1, img2)
+                        current_psnr = psnr
+                        current_mse = mse
+                    else:
+                        mse = current_mse
+                        psnr = current_psnr
+                    count += 1
+                    row.append(mse)
+                    row.append(psnr)
+                    data.append(row)
+                    writer.writerow(row)
             print(tabulate(data, headers=columns, tablefmt="grid", showindex="always"))
             new_path = os.path.join("..")
             os.chdir(new_path)
