bda52ee2dd337484e192fa94274337fc0dd78a68
Kristiyan Michaylov <krisidm24@gmail.com>
2023-06-05 12:15:27 +0200

    Add csv results

--- src/RQ3/CSV_results_analysis_per_tool.py
+++ src/RQ3/CSV_results_analysis_per_tool.py
@@ -21,46 +21,53 @@ columns = ["Name", "Color", "Mean Size", "Standard Deviation Size", "Deviation f
            "Standard Deviation MSE", "Deviation from non-stego MSE",
            "Mean PSNR", "Standard Deviation PSNR", "Deviation from non-stego PSNR"]
 
+
 for i in range(0, 3):
+    location_for_results = os.path.join("..", "RQ3_data_per_tool",
+                                        '{}.csv'.format(CSV_FILES[i][0].split("-")[0]))
     df = pd.concat(map(pd.read_csv, CSV_FILES[i]))
     table_data = []
-    for j in range(0, 31, 2):
-        row = []
-        elements = df.loc[j]
-        name_of_file = elements['Name'].tolist()
-        row.append(name_of_file[0])
-        color_of_file = elements['Color'].tolist()
-        row.append(color_of_file[0])
-        list_of_sizes = elements['Size'].tolist()
-        elements_with_only_numbers = [el.replace(" B", "") for el in list_of_sizes]
-        elements_converted_to_numbers = [float(el) for el in elements_with_only_numbers]
-        mean_size = np.mean(elements_converted_to_numbers)
-        row.append(mean_size)
-        standard_deviation_size = np.std(elements_converted_to_numbers)
-        row.append(standard_deviation_size)
-        non_stego_size = df.loc[j + 1]['Size'].tolist()[0].replace(" B", "")
-        non_stego_size = float(non_stego_size)
-        deviation_from_non_stego_size = [non_stego_size - x for x in elements_converted_to_numbers]
-        row.append(deviation_from_non_stego_size)
+    with open(location_for_results, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(columns)
+        for j in range(0, 31, 2):
+            row = []
+            elements = df.loc[j]
+            name_of_file = elements['Name'].tolist()
+            row.append(name_of_file[0])
+            color_of_file = elements['Color'].tolist()
+            row.append(color_of_file[0])
+            list_of_sizes = elements['Size'].tolist()
+            elements_with_only_numbers = [el.replace(" B", "") for el in list_of_sizes]
+            elements_converted_to_numbers = [float(el) for el in elements_with_only_numbers]
+            mean_size = np.mean(elements_converted_to_numbers)
+            row.append(mean_size)
+            standard_deviation_size = np.std(elements_converted_to_numbers)
+            row.append(standard_deviation_size)
+            non_stego_size = df.loc[j + 1]['Size'].tolist()[0].replace(" B", "")
+            non_stego_size = float(non_stego_size)
+            deviation_from_non_stego_size = [non_stego_size - x for x in elements_converted_to_numbers]
+            row.append(deviation_from_non_stego_size)
 
-        elements_converted_to_numbers_mse = [float(el) for el in elements['MSE'].tolist()]
-        mean_mse = np.mean(elements_converted_to_numbers_mse)
-        row.append(mean_mse)
-        standard_deviation_mse = np.std(elements_converted_to_numbers_mse)
-        row.append(standard_deviation_mse)
-        non_stego_mse = df.loc[j + 1]['MSE'].tolist()[0]
-        non_stego_mse = float(non_stego_mse)
-        deviation_from_non_stego_mse = [non_stego_mse - x for x in elements_converted_to_numbers_mse]
-        row.append(deviation_from_non_stego_mse)
+            elements_converted_to_numbers_mse = [float(el) for el in elements['MSE'].tolist()]
+            mean_mse = np.mean(elements_converted_to_numbers_mse)
+            row.append(mean_mse)
+            standard_deviation_mse = np.std(elements_converted_to_numbers_mse)
+            row.append(standard_deviation_mse)
+            non_stego_mse = df.loc[j + 1]['MSE'].tolist()[0]
+            non_stego_mse = float(non_stego_mse)
+            deviation_from_non_stego_mse = [non_stego_mse - x for x in elements_converted_to_numbers_mse]
+            row.append(deviation_from_non_stego_mse)
 
-        elements_converted_to_numbers_psnr = [float(el) for el in elements['PSNR'].tolist()]
-        mean_psnr = np.mean(elements_converted_to_numbers_psnr)
-        row.append(mean_psnr)
-        standard_deviation_psnr = np.std(elements_converted_to_numbers_psnr)
-        row.append(standard_deviation_psnr)
-        non_stego_psnr = df.loc[j + 1]['PSNR'].tolist()[0]
-        non_stego_psnr = float(non_stego_psnr)
-        deviation_from_non_stego_psnr = [non_stego_psnr - x for x in elements_converted_to_numbers_psnr]
-        row.append(deviation_from_non_stego_psnr)
-        table_data.append(row)
-    print(tabulate(table_data, headers=columns, tablefmt="grid", showindex="always"))
+            elements_converted_to_numbers_psnr = [float(el) for el in elements['PSNR'].tolist()]
+            mean_psnr = np.mean(elements_converted_to_numbers_psnr)
+            row.append(mean_psnr)
+            standard_deviation_psnr = np.std(elements_converted_to_numbers_psnr)
+            row.append(standard_deviation_psnr)
+            non_stego_psnr = df.loc[j + 1]['PSNR'].tolist()[0]
+            non_stego_psnr = float(non_stego_psnr)
+            deviation_from_non_stego_psnr = [non_stego_psnr - x for x in elements_converted_to_numbers_psnr]
+            row.append(deviation_from_non_stego_psnr)
+            table_data.append(row)
+            writer.writerow(row)
+        print(tabulate(table_data, headers=columns, tablefmt="grid", showindex="always"))
